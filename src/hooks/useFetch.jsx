9fbe7995bf23823800f772d50996bd124d3fc28d
NiranMike <olaniranmicheal2020@gmail.com>
2024-02-06 12:01:05 +0100

    feat: add useFetch hook for calling api

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url, options) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url, options)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data.data.rates);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url, options]);
+
+  return { data, error, loading };
+}
